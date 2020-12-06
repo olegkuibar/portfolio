9e918759ac2a70e184f6fe08070cbe8195c7e58f
Oleg Kuibar <kuibar.oleg@gmail.com>
2020-12-06 19:25:36 +0300

    Add route injector to the main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,12 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
 
+import 'main.template.dart' as self;
+
+@GenerateInjector(routerProvidersHash)
+final InjectorFactory appInjector = self.appInjector$Injector;
+
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: appInjector);
 }
