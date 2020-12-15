9ee56b27031c5c190f18fad88281107b77ab609d
Oleg Kuibar <kuibar.oleg@gmail.com>
2020-12-16 00:22:36 +0300

    fix: resolve issue with test providers; fix build

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,43 +1,47 @@
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:portfolio_angular_app/app_component.dart';
-import 'package:portfolio_angular_app/app_component.template.dart' as appTmp;
-import '../web/main.template.dart' as self;
+import 'package:portfolio_angular_app/app_component.template.dart' as ng;
 
 import 'package:angular_test/angular_test.dart';
+import 'package:portfolio_angular_app/heading_component.dart';
 import 'package:test/test.dart';
 
+import 'app_test.template.dart' as self;
 
 import 'Utils/injector-prob.dart';
 
-NgTestFixture<AppComponent> fixture;
+// note: pageloader packge is not compatible with current version of AngularDart
 
+@TestOn('browser')
 @GenerateInjector([
-  ClassProvider(RouterOutlet),
-  ClassProvider(RouterLink),
-  ClassProvider(RouterLinkActive)
+  ClassProvider(OkpHeadingComponent),
+  ClassProvider(Router, useClass: MockRouter),
+  routerDirectives,
 ])
 final InjectorFactory rootInjector = self.rootInjector$Injector;
 
 void main() {
   final injector = InjectorProbe(rootInjector);
+
   final testBed =
-      NgTestBed.forComponent<AppComponent>(appTmp.AppComponentNgFactory, rootInjector: injector.factory);
+      NgTestBed.forComponent<AppComponent>(ng.AppComponentNgFactory, rootInjector: injector.factory);
+  NgTestFixture<AppComponent> fixture;
 
   setUp(() async {
     fixture = await testBed.create();
   });
 
-
   tearDown(disposeAnyRunningTest);
   
   test('Should contain three sections', () {
-    final sections = fixture.rootElement.getElementsByClassName('section');
-    int count = 0;
-
-    sections.forEach((element) => count++ );
-
-    expect(count, 3);
+    final sectionCount = fixture.rootElement.children.first.children.length; // dirty way to access 'section' node count
+    expect(sectionCount, 3);
   });
 }
+
+class MockRouter extends Mock implements Router {}
