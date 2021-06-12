6b56ee3cb92207d9993eb72400b8e201401914df
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-06-12 17:16:48 +0300

    feat: update main.ts
    add font-awesome svg icons
    
    * solid "faSun"
    * brand "faGithub", "faVuejs", "faJs"

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// @ts-ignore
+import VTooltipPlugin from "v-tooltip";
+
+import "./index.scss";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faJs, faVuejs } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faSun, faGithub, faVuejs, faJs);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(VTooltipPlugin)
+  .mount("#app");
