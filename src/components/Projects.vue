9b55c91485158dd5bed141d88c7b0684bcc30522
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-06-12 17:13:40 +0300

    feat: add projects component

---
+++ src/components/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi
+  deleniti dolorum eum labore, minus nemo omnis optio quas quia saepe sapiente
+  suscipit voluptate. Aperiam blanditiis, cupiditate deleniti, dolorum harum
+  illo ipsa ipsam iusto labore modi neque nihil nostrum odit quod reiciendis
+  soluta temporibus unde vel velit, veniam voluptas voluptate?
+</template>
+
+<script>
+export default {
+  name: "Projects",
+};
+</script>
+
+<style scoped></style>
