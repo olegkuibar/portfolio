eaf6c3bf114bed20b7b7a511d36bf93f0fe7f9d8
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-06-12 17:13:33 +0300

    feat: add profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consectetur
+    excepturi minus nobis obcaecati, odio omnis? Blanditiis dolorem hic id
+    magnam, nobis optio quas qui quo. Adipisci aperiam consectetur enim incidunt
+    obcaecati officiis optio, quod sunt totam unde. Aliquid dignissimos dolore
+    ea eaque esse iste non, numquam placeat quam suscipit.
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+};
+</script>
+
+<style scoped>
+* {
+  @apply text-black dark:text-gray-100;
+}
+</style>
